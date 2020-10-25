79bb71cb7e8714ac13ade8aabad6af20323adb86
rinalistiana <46235415+rinalistiana@users.noreply.github.com>
2020-10-25 19:26:59 +0700

    Add files via upload

---
+++ Folder 2/Praktikum 1/Perkenalan.php
@@ -0,0 +1,12 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h3>Perkenalkan</h3>";
+        echo " Nama saya Rina Listiana, 
+        	   Saya tinggal di Tegal, 
+        	   Saya suka mendengarkan lagu";
+     
+    }
+}
